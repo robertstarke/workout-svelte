95e4cf8994fa5cebcb6e6a416722767022a388aa
Robert Starke <mail@rstarke.de>
2024-02-12 23:30:22 -0600

    init svelte kit and tailwind, created data json and imported it into app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	import '../app.css';
+	import excerciseData from '$lib/data/exercises.json';
+
+	const excercises = writable(
+		excerciseData.map((excercise) => ({ ...excercise, selectedForWorkout: false }))
+	);
+	setContext('excercises', excercises);
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { getContext } from 'svelte';
+
+	const excercises = getContext('excercises');
+</script>
+
+{#each $excercises as excercise}
+	<div>{excercise.name}</div>
+{/each}
