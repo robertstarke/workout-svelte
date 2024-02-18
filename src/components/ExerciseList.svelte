7d83eee0873e085286dd9d8d53aeae4758fdec9e
Robert Starke <mail@rstarke.de>
2024-02-18 00:04:03 -0600

    finished styling workout setup

--- src/components/ExerciseList.svelte
+++ src/components/ExerciseList.svelte
@@ -1,28 +1,46 @@
 <script>
 	import { flip } from 'svelte/animate';
 	import { send, receive } from '../animation/exerciseTransition';
+	import { Square, CheckSquare } from 'lucide-svelte';
 
 	export let exercises;
 	export let selected = false;
 </script>
 
-<ul>
+<ul class="space-y-4">
 	{#each $exercises.filter((e) => e.selected === selected) as exercise (exercise.id)}
 		<li
-			class:selected
 			in:receive={{ key: exercise.id }}
 			out:send={{ key: exercise.id }}
 			animate:flip={{ duration: 200 }}
 		>
-			<label for={exercise.id}>
+			<label
+				for={exercise.id}
+				class="w-full h-full flex flex-row justify-between items-stretch bg-zinc-900 rounded-md cursor-pointer"
+			>
 				<input
 					type="checkbox"
+					class="hidden peer"
 					checked={exercise.selected}
 					id={exercise.id}
 					name={selected ? 'selectedExercises' : 'exercises'}
 					on:change={(e) => exercises.select(exercise, e.currentTarget.checked)}
 				/>
-				{exercise.name}
+				<span class="px-4 py-2">
+					<span class="block text-xl text-rose-500">{exercise.name}</span>
+					<span class="block text-sm text-stone-50">{exercise.description}</span>
+				</span>
+				<span
+					class="flex-none flex items-center justify-center w-16 rounded-r-md bg-stone-400 text-zinc-800 peer-checked:bg-rose-500"
+				>
+					<span class="text-4xl">
+						{#if selected}
+							<CheckSquare />
+						{:else}
+							<Square />
+						{/if}
+					</span>
+				</span>
 			</label>
 		</li>
 	{/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { getContext } from 'svelte';
 	import { fade } from 'svelte/transition';
+	import { Circle, CheckCircle } from 'lucide-svelte';
+
 	import ExerciseList from '../components/ExerciseList.svelte';
 
 	const exercises = getContext('exercises');
@@ -13,101 +15,114 @@
 	$: workoutLength = (exerciseLength + restLength) * selectedExercisesAmount * repetitions;
 </script>
 
-<div class="mx-auto p-8 max-w-screen-lg">
-	<section>
-		<div class="flex flex-col gap-4 items-center">
+<div class="mx-auto p-8">
+	<section class="mt-4 grid grid-cols-5 gap-8">
+		<div class="col-start-2 col-span-3">
 			<button
-				class="w-80 p-4 bg-rose-500 rounded-md text-zinc-800 text-2xl disabled:bg-stone-400 transition-colors hover:bg-rose-600 hover:text-zinc-900"
+				class="block w-full p-4 bg-rose-500 rounded-md text-zinc-800 text-2xl disabled:bg-stone-400 transition-colors hover:bg-rose-600 hover:text-zinc-900"
 				disabled={selectedExercisesAmount === 0}>Start Workout</button
 			>
-			<span class="block w-80 p-4 bg-zinc-900 rounded-md text-4xl text-rose-500 text-center">
+		</div>
+		<div class="col-span-1 col-start-2 row-start-2">
+			<div class="block w-full p-4 bg-zinc-900 rounded-md text-4xl text-rose-500 text-center">
 				<span class="block mb-2 text-2xl">Total Workout Time</span>
 				{#if workoutLength / 3600 >= 1}{Math.floor(workoutLength / 3600)}h{/if}
 				{#if workoutLength % 3600 !== 0}{Math.floor((workoutLength % 3600) / 60)}m{/if}
 				{#if workoutLength % 60 !== 0}{workoutLength % 60}s{/if}
 				{#if workoutLength === 0}N/A{/if}
-			</span>
-		</div>
-	</section>
-	<section class="mt-4 flex flex-col gap-4">
-		<label for="exerciseLength" class="flex flex-row justify-center items-center gap-4">
-			<span class="w-28 p-4 bg-zinc-900 rounded-md text-4xl text-rose-500 text-center"
-				>{exerciseLength}s</span
-			>
-			<span class="flex flex-col">
-				<span class="text-xl">Exercise Length</span>
-				<input
-					id="exerciseLength"
-					type="range"
-					step="5"
-					min="10"
-					max="180"
-					bind:value={exerciseLength}
-				/>
-			</span>
-		</label>
-		<label for="restLength" class="flex flex-row justify-center items-center gap-4">
-			<span class="w-28 p-4 bg-zinc-900 rounded-md text-4xl text-rose-500 text-center"
-				>{restLength}s</span
-			>
-			<span class="flex flex-col">
-				<span class="text-xl">Rest Length</span>
-				<input id="restLength" type="range" step="5" min="0" max="120" bind:value={restLength} />
-			</span>
-		</label>
-		<label for="repetitions" class="flex flex-row justify-center items-center gap-4">
-			<span class="w-28 p-4 bg-zinc-900 rounded-md text-4xl text-rose-500 text-center"
-				>{repetitions}</span
-			>
-			<span class="flex flex-col">
-				<span class="text-xl">Repetitions</span>
-				<input id="repetitions" type="range" step="1" min="1" max="10" bind:value={repetitions} />
-			</span>
-		</label>
-		{#if repetitions > 1}
-			<div transition:fade={{ duration: 200 }}>
-				<div class="flex flex-row justify-center items-stretch justify-items-stretch gap-4">
-					<label
-						for="set"
-						class="p-4 bg-zinc-900 rounded-md text-3xl text-rose-500 text-center cursor-pointer has-[:checked]:bg-rose-500 has-[:checked]:text-zinc-800"
-					>
-						<input
-							type="radio"
-							id="set"
-							value="set"
-							name="setOrCycle"
-							bind:group={setOrCycle}
-							class="hidden"
-						/>
-						Set
-					</label>
-					<label
-						for="cycle"
-						class="p-4 bg-zinc-900 rounded-md text-3xl text-rose-500 text-center cursor-pointer has-[:checked]:bg-rose-500 has-[:checked]:text-zinc-800"
-					>
-						<input
-							type="radio"
-							id="cycle"
-							value="cycle"
-							name="setOrCycle"
-							bind:group={setOrCycle}
-							class="hidden"
-						/>
-						Cycle
-					</label>
-				</div>
 			</div>
-		{/if}
-	</section>
-	<section class="flex flex-row justify-center gap-6 mt-6">
-		<div>
-			<h2>Available exercises</h2>
-			<ExerciseList {exercises} selected={false} />
+			<div class="mt-4 flex flex-col gap-4">
+				<label for="exerciseLength" class="flex flex-row items-center gap-4">
+					<span class="w-28 p-4 bg-zinc-900 rounded-md text-4xl text-rose-500 text-center"
+						>{exerciseLength}s</span
+					>
+					<span class="flex flex-col">
+						<span class="text-xl">Exercise Length</span>
+						<input
+							id="exerciseLength"
+							type="range"
+							step="5"
+							min="10"
+							max="180"
+							bind:value={exerciseLength}
+						/>
+					</span>
+				</label>
+				<label for="restLength" class="flex flex-row items-center gap-4">
+					<span class="w-28 p-4 bg-zinc-900 rounded-md text-4xl text-rose-500 text-center"
+						>{restLength}s</span
+					>
+					<span class="flex flex-col">
+						<span class="text-xl">Rest Length</span>
+						<input
+							id="restLength"
+							type="range"
+							step="5"
+							min="0"
+							max="120"
+							bind:value={restLength}
+						/>
+					</span>
+				</label>
+				<label for="repetitions" class="flex flex-row items-center gap-4">
+					<span class="w-28 p-4 bg-zinc-900 rounded-md text-4xl text-rose-500 text-center"
+						>{repetitions}</span
+					>
+					<span class="flex flex-col">
+						<span class="text-xl">Repetitions</span>
+						<input
+							id="repetitions"
+							type="range"
+							step="1"
+							min="1"
+							max="10"
+							bind:value={repetitions}
+						/>
+					</span>
+				</label>
+				{#if repetitions > 1}
+					<div transition:fade={{ duration: 200 }}>
+						<div class="grid grid-cols-2 items-stretch justify-items-stretch gap-4">
+							{#each ['set', 'cycle'] as repetition}
+								<label
+									for={repetition}
+									class="flex flex-row justify-between items-stretch bg-zinc-900 rounded-md text-2xl text-rose-500 cursor-pointer"
+								>
+									<input
+										type="radio"
+										id={repetition}
+										value={repetition}
+										name="setOrCycle"
+										bind:group={setOrCycle}
+										class="hidden peer"
+									/>
+									<span class="p-4">{repetition[0].toUpperCase()}{repetition.slice(1)}</span>
+									<span
+										class="flex-none flex items-center justify-center w-16 rounded-r-md bg-stone-400 text-zinc-800 peer-checked:bg-rose-500"
+									>
+										{#if setOrCycle === repetition}
+											<CheckCircle />
+										{:else}
+											<Circle />
+										{/if}
+									</span>
+								</label>
+							{/each}
+						</div>
+					</div>
+				{/if}
+			</div>
 		</div>
+		<div class="grid grid-cols-subgrid col-span-2 justify-center gap-8">
+			<div>
+				<h2 class="mb-4 text-2xl text-zinc-800">Available Exercises</h2>
+				<ExerciseList {exercises} selected={false} />
+			</div>
 
-		<div>
-			<h2>Selected exercises</h2>
-			<ExerciseList {exercises} selected={true} />
+			<div>
+				<h2 class="mb-4 text-2xl text-zinc-800">Selected Exercises</h2>
+				<ExerciseList {exercises} selected={true} />
+			</div>
 		</div>
 	</section>
 </div>
