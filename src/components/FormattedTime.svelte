e52ba688c09f185299a5888539d952a0f32e8606
Robert Starke <mail@rstarke.de>
2024-04-01 12:31:29 -0500

    seconds are now always shown in formatted time

--- src/components/FormattedTime.svelte
+++ src/components/FormattedTime.svelte
@@ -6,6 +6,6 @@
 <span>
 	{#if timeInSeconds / 3600 >= 1}{Math.floor(timeInSeconds / 3600)}h{/if}
 	{#if (timeInSeconds % 3600) / 60 >= 1}{Math.floor((timeInSeconds % 3600) / 60)}m{/if}
-	{#if timeInSeconds % 60 !== 0}{timeInSeconds % 60}s{/if}
+	{#if timeInSeconds > 0}{timeInSeconds % 60}s{/if}
 	{#if timeInSeconds === 0}0s{/if}
 </span>
